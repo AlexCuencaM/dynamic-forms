72cac40fe1ac1b03d54d9b1d6ba4c3e42485cf25
Alex Cuenca M <acuenca@apptelink.com>
2024-07-04 18:03:59 -0500

    renaming file

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import { CustomTabPanel } from './ItemTabs';
+import { CustomTabPanel } from './CustomTabPanel';
 import { IOptionsRepository } from '@/data/Interfaces/IOptionsRepository';
 import { useEffect, useState } from 'react';
 import { Options } from '@/data/Entities/Options';
