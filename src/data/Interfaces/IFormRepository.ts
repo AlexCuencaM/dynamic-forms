6d7ec10f258be0461a28b8fd2f7aff731f1ceb71
Alex Cuenca M <acuenca@apptelink.com>
2024-07-04 17:29:12 -0500

    Added readme

--- src/data/Interfaces/IFormRepository.ts
+++ src/data/Interfaces/IFormRepository.ts
@@ -3,7 +3,4 @@ import { Form } from "../Entities/Form";
 
 export interface IFormRepository{
     getAsync(optionId: number):Promise<Form>;
-    // postAsync(form:Form):Promise<MessageInfoDTO>;
-    // putAsync(form:Form):Promise<MessageInfoDTO>;
-    // deleteAsync(formId:number):Promise<MessageInfoDTO>;
 }
