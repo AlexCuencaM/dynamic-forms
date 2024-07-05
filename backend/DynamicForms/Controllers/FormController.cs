a48b86b7505621a77a976a2b22b65826fb9f16c1
Alex Cuenca M <acuenca@apptelink.com>
2024-07-05 09:00:19 -0500

    adding backend

---
+++ backend/DynamicForms/Controllers/FormController.cs
@@ -0,0 +1,26 @@
+﻿using DynamicForms.Interfaces;
+using DynamicForms.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicForms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController(IFormRepository repository) : ControllerBase
+    {
+        private readonly IFormRepository _repository = repository;
+        [HttpGet("{optionId}")]
+        public async Task<IActionResult> GetAsync(int optionId)
+        {
+            try
+            {
+                var result = await _repository.GetAsync(optionId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ backend/DynamicForms/Interfaces/IFormRepository.cs
@@ -0,0 +1,11 @@
+﻿using DynamicForms.Models.DTOs;
+
+namespace DynamicForms.Interfaces;
+
+public interface IFormRepository
+{
+    Task<FormDTO> GetAsync(int optionId);
+    Task<MessageInfoDTO> PostAsync(FormDTO form);
+    Task<MessageInfoDTO> PutAsync(FormDTO form);
+    Task<MessageInfoDTO> DeleteAsync(int formId);
+}
